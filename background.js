ca9da042409f848b7e41a0bee97194a67d50c48f
sdob <sdob@cs.ucc.ie>
2017-03-09 16:49:33 +0000

    Disable background page-action stuff for now

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+/*
 chrome.runtime.onInstalled.addListener(() => {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
@@ -14,3 +15,4 @@ chrome.runtime.onInstalled.addListener(() => {
     ]);
   });
 });
+*/
